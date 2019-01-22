285d3e3c7496623287e175a46a0e815b5641dca9
Alexis-PJ <44810566+Alexis-PJ@users.noreply.github.com>
2019-01-22 08:00:43 -0700

    Add files via upload

--- alex_mylibformulas.py
+++ alex_mylibformulas.py
@@ -21,3 +21,33 @@ def swap(a,b):
     a = b
     b = temp
     return print(a,b)
+
+def distance2d(x1,y1,x2,y2):
+    d = math.sqrt((x1 -x2)**2 + (y1 -y2)**2)
+    return d
+
+def distance3d(x1,y1,z1,x2,y2,z2):
+    b = math.sqrt((x1 -x2)**2 + (y1 -y2)**2 + (z1 -z2)**2)
+    return b
+
+def eliminate_repeats(oldlist):
+    newlist = []
+    for letter in oldlist:
+        if letter not in newlist:
+            newlist.append(letter)
+    return newlist
+
+def convert_list_2word(alist):
+    n = len(alist)
+    new_word = ''
+    for i in range(n):
+        new_word = new_word + alist[i]
+    return new_word
+
+def translate(text1,dict1):
+    list_text = text1.split()
+    new_list =[]
+    for word in list_text:
+        translation = dict1.get(word)
+        new_list.append(translation if translation else word)
+    return ' '.join(new_list)
